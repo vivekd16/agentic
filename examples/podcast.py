32f9962c63f9ae7715cefb07f407f0157b31cd90
Scott Persinger <scottpersinger@gmail.com>
2025-02-03 09:34:24 -0500

    fixed prompts

--- examples/podcast.py
+++ examples/podcast.py
@@ -5,23 +5,18 @@ from agentic.tools.auth_rest_api_tool import AuthorizedRESTAPITool
 producer = Agent(
     name="Podcast Producer",
     welcome="I am a podcast producer. I can produce a daily news podcast.",
-    instructions="""
-1. Call the AI News Reporter to get the daily news report.
-2. Now convert the report into speech, choosing one of the available voices at random.
-3. now call the Transistor agent, ask it to create and then publish a new episode using the audio link and naming the episode "Latest AI news from <date>" and add the date.
-4. Print the episode ID returned
-""",    
+    instructions="{{PRODUCER}}",
     model="gpt-4o-mini",
     functions=[TextToSpeechTool()],
 )
 producer.add_child(
     name="AI News Reporter",
-    instructions="{{PRODUCER}}",
+    instructions=f"{{REPORTER}}",
     model="openai/gpt-4o",    
 )
 producer.add_child(
     name="Transistor",
-    instructions="{{REPORTER}}",
+    instructions="{{TRANSITOR}}",
     functions=[AuthorizedRESTAPITool()],
     memories=["Default show ID is 60214"],
 )

da80805cf641f8b1612b1825ba6507810b87d072
annnniexu <112122685+annnniexu@users.noreply.github.com>
2025-04-21 07:43:27 -0700

    Podcast Agent and Article Scraper (#149)
    * article scraper and podcast agent
    
    * cleaned up
    
    * edit numbers
    
    * Renamed the tool and moved the podcast agent into examples
    
    * Added the podcast tool to the tools/__init__.py file
    
    * Added __init__.pyi for type references for tools, added test
    
    ---------
    
    Co-authored-by: Declan Brady <drbrady8800@gmail.com>

---
+++ examples/podcast_long.py
@@ -0,0 +1,108 @@
+
+from agentic.common import Agent, AgentRunner
+from agentic.tools import AuthorizedRestApiTool, PodcastTool, TextToSpeechTool
+
+from agentic.models import GPT_4O
+from agentic.common import Agent, AgentRunner
+
+from datetime import datetime
+from pydub import AudioSegment
+import json
+import traceback
+import os
+
+
+def create_combined_podcast():
+    """
+    Generates a combined podcast audio file by scraping articles from the given list of news segment URLs,
+    summarizing their content, converting them to audio, and concatenating them into one MP3 file.
+    """
+    segment_sites=["https://www.nbcnews.com/news/us-news", "https://www.nbcnews.com/news/world", "https://www.nbcnews.com/tech"]
+    base_url='https://e4ed-73-15-6-205.ngrok-free.app'
+    
+    ast = PodcastTool()
+
+    audio_dir = os.path.join(os.path.dirname(__file__), "audios")
+    os.makedirs(audio_dir, exist_ok=True)
+
+    file_path = os.path.dirname(os.getcwd()) + "/article_scraper_transcript.txt"
+    audio_files = []
+    try:
+        for site in segment_sites:
+            ast.news_scrape_and_download(news_site=site, save_file=file_path, num_articles=4, is_summarize=True, sum_wc=500)
+            audio_file = ast.create_podcast_from_text(file_path)
+            audio_files.append(audio_file)
+        combined = AudioSegment.empty()
+        for f in audio_files:
+            file_info = json.loads(f)
+            audio_url = file_info["audio_url"]
+            audio_url = audio_url.split("file:///")[-1]
+            combined += AudioSegment.from_mp3(audio_url)
+        
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"news_podcast_{timestamp}.mp3"
+
+        output_path = os.path.join(audio_dir, filename)
+        combined.export(output_path, format="mp3")
+
+        #devtunnel
+        public_audio_url = f"{base_url}/{filename}"
+
+        return json.dumps(
+            {"content_type": "audio/mpeg", "audio_url": public_audio_url}
+        )
+
+    except Exception as e:
+        traceback.print_exc()
+        error_message = f"Error generating combined podcast: {str(e)}"
+        return json.dumps({"error": error_message})
+
+
+#transistor fm agent
+tfm_inst = """
+  You are a helpful assistant. You can call the Transistor API which is used for managing a podcast.
+  Here are the API endpoints:
+
+  The top level object is a "Show", and each show contains Episodes.
+
+  GET https://api.transistor.fm/v1 - Return the current authorized user
+  GET /v1/shows - Gets the list of Shows
+  GET /v1/episodes - Gets a list episodes for a show. Takes show_id parameter, and optional "status" filter parameter (one of draft, scheduled, published)
+
+  POST /v1/episodes - To create a new episode. Takes these parameters:
+    episode[show_id] 
+    episode[audio_url]
+    episode[title]
+
+  PATCH /v1/episodes/{id}/publish - To publish an episode. Takes these parameters:
+    id - episode ID
+    episode[status] - one of draft, published, scheduled
+"""
+#transistor fm agent
+upload_agent = Agent(
+    name="TransistorFM",
+    welcome="I can work with podcast episodes via the Transistor.fm API.",
+    instructions=tfm_inst,
+    tools=[AuthorizedRestApiTool("header", "TRANSISTOR_API_KEY", "x-api-key")],
+    memories=["Default show ID is 60214"],
+    model=GPT_4O,
+    )
+
+
+inst_comb = """
+When started, immedietly execute the following:
+1. Call create_combined_podcast to create a combined audio and get the public audio link
+2. Call the upload agent, ask it to create and then publish a DRAFT episode using the public audio link on Transistor FM and naming the episode "News Recap from <date> and add the date.
+"""
+
+agent = Agent(
+    name="Podcast Agent",
+    instructions=inst_comb,
+    model=GPT_4O,
+    tools=[PodcastTool(), TextToSpeechTool(), upload_agent, create_combined_podcast]
+)
+
+
+if __name__ == "__main__":
+    AgentRunner(agent).repl_loop()
+        
---
+++ src/agentic/tools/podcast_tool.py
@@ -0,0 +1,108 @@
+from typing import Callable
+
+from agentic.tools.text_to_speech_tool import TextToSpeechTool
+from .base import BaseAgenticTool
+
+import requests
+from bs4 import BeautifulSoup
+import openai
+
+class PodcastTool(BaseAgenticTool):
+    def __init__(self):
+        super().__init__()
+
+    def get_tools(self) -> list[Callable]:
+        return [
+            self.download_and_write_article,
+            self.gather_urls,
+            self.summarize,
+            self.news_scrape_and_download,
+            self.create_podcast_from_text
+        ]
+    
+
+    def download_and_write_article(
+        self,
+        download_url:str,
+        save_file: str,
+        is_summarize: bool = True,
+        sum_wc: int = 500
+    ):
+        r = requests.get(download_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        content = ""
+        success = False
+        with open(save_file, "a") as f:
+            for title in soup.find_all('title'):
+                if len(title.get_text().split()) > 3:
+                    ttext = title.get_text()
+            for data in soup.find_all("p"):
+                content += data.get_text() + '\n'
+            if len(content) > 3000:
+                if is_summarize:
+                    f.write(self.summarize(article=content, wordcount=sum_wc, title=ttext))   
+                else:
+                    f.write("\n\n"+ ttext + "\n")
+                    f.write(content) 
+                success = True
+        return success
+    
+
+    def summarize(
+        self,
+        article: str,
+        wordcount: int,
+        title: str, 
+        model="gpt-4o"
+    ):
+        client = openai.OpenAI()
+        prompt = "Please summarize the following article titled " + title+ " in detail in " + str(wordcount) + " words in the style of a on-air news reporter. Include a introduction/transition sentence in the beginning: \n"
+        prompt += article
+        messages = [{"role": "user", "content": prompt}]
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=0,
+        )
+        res = response.choices[0].message.content
+        return res
+
+## PODCAST SPECIFIC FUNCTIONS
+    def gather_urls(
+        self,
+        site_url: str
+    ):
+        r = requests.get(site_url)
+        urls = set()
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if site_url in href: #validates news link
+                urls.add(href)
+        return urls    
+    
+    def news_scrape_and_download(
+        self,
+        news_site: str,
+        save_file: str,
+        num_articles: int = 5,
+        is_summarize: bool = True,
+        sum_wc: int = 500
+    ):
+        with open(save_file, "w") as f:
+            f.write("")
+        urls = list(self.gather_urls(news_site))
+        count = 0
+        while count < num_articles and len(urls) > 0:
+            curl = urls.pop(0)
+            success = self.download_and_write_article(curl, save_file, True)
+            if success:
+                count += 1
+    
+    def create_podcast_from_text(
+        self,
+        save_file: str
+    ):
+        tts = TextToSpeechTool()
+        res = tts.generate_speech_file_from_text(voice="nova", input_file_name=save_file)
+        return(res)

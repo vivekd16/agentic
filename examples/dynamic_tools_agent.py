6be3a7152e7bed74dbf4ed90f9e824eed0f941cb
emmakodes <emmamichael65@gmail.com>
2025-04-23 15:04:23 +0100

    used imported GPT_4O_MINI instead

--- examples/dynamic_tools_agent.py
+++ examples/dynamic_tools_agent.py
@@ -12,6 +12,7 @@ from agentic.tools import (
 
 from agentic.common import Agent, AgentRunner
 from agentic.tools.utils.registry import tool_registry
+from agentic.models import GPT_4O_MINI
 
 # This is a demonstration of dynamic tool use. You can add a tool to an agent at any time. This
 # demo uses a meta tool called "AutomaticTools" which will automatically load tools from the
@@ -27,7 +28,7 @@ agent = Agent(
     name="Dynamic Tools Agent",
     welcome="I have a list of tools which I can enable and use on-demand. Query the list of tools to start.",
     instructions="You are a helpful assistant. You can list tools or enable a tool for different purposes.",
-    model="openai/gpt-4o-mini",
+    model=GPT_4O_MINI,
     tools=[
         AutomaticTools(
             tool_classes=[
